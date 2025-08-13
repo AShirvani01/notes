9d9fb15d6aee24aad435a6e7e0962f5472f5e97f
Abtin <abtin.kit.shirvani@gmail.com>
2025-08-13 13:49:39 -0400

    Quartz sync: Aug 13, 2025, 1:49 PM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,34 +1,35 @@
 import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
+import { joinSegments } from "../util/path"
 import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = joinSegments(baseDir, "static/logo.png")
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <img src="../../content/Assets/spacedog.png" alt="icon" class="icon" />
-      <a href={baseDir}>{title}</a>
-    </h2>
+    <h1 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>
+        <img class="Logo" src={iconPath} alt={title} />
+      </a>
+    </h1>
   )
 }
 
 PageTitle.css = `
 .page-title {
-  font-size: 1.75rem;
   margin: 0;
-  font-family: var(--titleFont);
 }
-.page-title .icon {
-  margin-right: 0.5rem;
-  width: 60px;
-  height: 60px;
+.Logo {
+max-height: 650px;
+min-height: 50px;
+max-width: 240px;
+min-width: 50px;
 }
 `
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
-
 
 /* ORIGINAL
 import { pathToRoot } from "../util/path"
@@ -53,4 +54,6 @@ PageTitle.css = `
   font-family: var(--titleFont);
 }
 `
+
+export default (() => PageTitle) satisfies QuartzComponentConstructor
 */
